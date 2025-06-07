60412f336d87fac5e5bfb3741c6fcc8d9636638c
Salah_Eddine_Saadani <salaheddinesaadani@gmail.com>
2025-06-07 19:18:38 +0200

    Add files via upload

---
+++ SINGLY LINKED LIST OPERATION - DELETE FIRST NODE/SINGLY LINKED LIST OPERATION - DELETE FIRST NODE/SINGLY LINKED LIST OPERATION - DELETE FIRST NODE.cpp
@@ -0,0 +1,174 @@
+// SINGLY LINKED LIST OPERATION - DELETE FIRST NODE.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+// Create a node
+class Node
+{
+public:
+    int value;
+    Node* next;
+};
+
+void InsertAtBeginning(Node*& head, int value)
+{
+    // Allocate memory to a node
+    Node* new_node = new Node();
+
+    // insert the data
+    new_node->value = value;
+    new_node->next = head;
+
+    // Move head to new node
+    head = new_node;
+
+}
+
+Node* Find(Node* head, int Value)
+{
+
+    while (head != NULL)
+    {
+
+        if (head->value == Value)
+            return head;
+
+        head = head->next;
+    }
+
+    return NULL;
+
+}
+
+// Insert a node after a node
+void InsertAfter(Node* Prev_node, int Value)
+{
+
+    if (Prev_node == NULL)
+    {
+        cout << "the given Previous node cannot be NULL";
+        return;
+    }
+
+    Node* new_node = new Node();
+    new_node->value = Value;
+    new_node->next = Prev_node->next;
+    Prev_node->next = new_node;
+}
+
+// Insert at the end
+void InsertAtEnd(Node*& head, int Value)
+{
+
+    Node* new_node = new Node();
+
+    new_node->value = Value;
+    new_node->next = NULL;
+
+    if (head == NULL) {
+        head = new_node;
+        return;
+    }
+
+    Node* LastNode = head;
+    while (LastNode->next != NULL)
+    {
+        LastNode = LastNode->next;
+    }
+
+    LastNode->next = new_node;
+    return;
+}
+
+// Delete a node
+void DeleteNode(Node*& head, int Value)
+{
+    Node* Current = head, * Prev = head;
+
+    if (head == NULL)
+    {
+        return;
+    }
+
+    if (Current->value == Value)
+    {
+        head = Current->next;
+        delete Current;//free from memory
+        return;
+    }
+
+    // Find the key to be deleted
+    while (Current != NULL && Current->value != Value)
+    {
+        Prev = Current;
+        Current = Current->next;
+    }
+
+    // If the value is not present
+    if (Current == NULL) return;
+
+    // Remove the node
+    Prev->next = Current->next;
+    delete Current;//free from memory
+
+}
+
+// Delete First Node node
+void DeleteFirstNode(Node*& head)
+{
+
+    Node* Current = head;
+
+    if (head == NULL)
+    {
+        return;
+    }
+
+    head = Current->next;
+    delete Current;//free from memory
+    return;
+
+}
+// Print the linked list
+void PrintList(Node* head)
+{
+    cout << "\n";
+    while (head != NULL)
+    {
+        cout << head->value << " ";
+        head = head->next;
+    }
+}
+
+int main()
+{
+    Node* head = NULL;
+
+    InsertAtEnd(head, 1);
+    InsertAtEnd(head, 2);
+    InsertAtEnd(head, 3);
+    InsertAtEnd(head, 4);
+    InsertAtEnd(head, 5);
+    InsertAtEnd(head, 6);
+    PrintList(head);
+
+
+    DeleteFirstNode(head);
+
+    PrintList(head);
+
+    system("pause>0");
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
