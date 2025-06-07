fc5a7397cb798597acf0a0fb2d3e5683dc96ac68
Salah_Eddine_Saadani <salaheddinesaadani@gmail.com>
2025-06-07 19:59:43 +0200

    Add files via upload

---
+++ SINGLY LINKED LIST OPERATION - FIND NODE/SINGLY LINKED LIST OPERATION - FIND NODE/SINGLY LINKED LIST OPERATION - FIND NODE.cpp
@@ -0,0 +1,87 @@
+// SINGLY LINKED LIST OPERATION - FIND NODE.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+// Create a node
+class Node
+{
+public:
+    int value;
+    Node* next;
+};
+
+void InsertAtBeginning(Node*& head, int value)
+{
+    // Allocate memory to a node
+    Node* new_node = new Node();
+
+    // insert the data
+    new_node->value = value;
+    new_node->next = head;
+
+    // Move head to new node
+    head = new_node;
+
+}
+
+Node* Find(Node* head, int Value)
+{
+
+    while (head != NULL)
+    {
+
+        if (head->value == Value)
+            return head;
+
+        head = head->next;
+    }
+
+    return NULL;
+
+}
+
+// Print the linked list
+void PrintList(Node* head)
+{
+    cout << "\n";
+    while (head != NULL)
+    {
+        cout << head->value << " ";
+        head = head->next;
+    }
+}
+int main()
+{
+    Node* head = NULL;
+
+    InsertAtBeginning(head, 1);
+    InsertAtBeginning(head, 2);
+    InsertAtBeginning(head, 3);
+    InsertAtBeginning(head, 4);
+    InsertAtBeginning(head, 5);
+
+    PrintList(head);
+
+    Node* N1 = Find(head, 2);
+
+    if (N1 != NULL)
+        cout << "\nNode Found :-)\n";
+    else
+        cout << "\nNode Is not found :-(\n";
+
+
+    system("pause>0");
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
