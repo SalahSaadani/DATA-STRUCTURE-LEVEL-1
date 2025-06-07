e466f1e68572b9061e40fd5452d8e4933205326b
Salah_Eddine_Saadani <salaheddinesaadani@gmail.com>
2025-06-07 13:06:45 +0200

    Add files via upload

---
+++ UNION DATA TYPE/UNION DATA TYPE/UNION DATA TYPE.cpp
@@ -0,0 +1,37 @@
+// UNION DATA TYPE.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+union MyUnion 
+{
+    int intValue;
+    float floatValue;
+    char charValue;
+};
+int main()
+{
+    MyUnion myUnion;
+
+    myUnion.intValue = 42;
+    std::cout << "Integer value: " << myUnion.intValue << std::endl;
+
+    myUnion.floatValue = 3.14f;
+    std::cout << "Float value: " << myUnion.floatValue << std::endl;
+
+    myUnion.charValue = 'A';
+    std::cout << "Char value: " << myUnion.charValue << std::endl;
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
